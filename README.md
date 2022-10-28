a9faf56c668b7f6bd30977f8a60db8d8046807ef
RafPlayz69YT <79861112+brosomethingwrongwithyothing@users.noreply.github.com>
2022-10-28 08:14:46 +0100

    comments

---
+++ README.md
@@ -0,0 +1,5 @@
+# this inverts your screen when you press a key
+
+seriously what did you expect?
+
+this has comments every where so you know whats going on
--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@
 
 int invertScreen()
 {
-    HDC desktop;
+    HDC desktop; // vars to store stuff
     HWND desktopHandle;
     RECT screenSize;
     screenSize.left = 0;
     screenSize.top = 0;
-    screenSize.right = GetSystemMetrics(SM_CXVIRTUALSCREEN);
-    screenSize.bottom = GetSystemMetrics(SM_CYVIRTUALSCREEN);
-    desktopHandle = GetDesktopWindow();
-    desktop = GetDC(desktopHandle);
-    InvertRect(desktop, &screenSize);
-    return ReleaseDC(desktopHandle, desktop);
+    screenSize.right = GetSystemMetrics(SM_CXVIRTUALSCREEN);  // width of screen 1 (i think)
+    screenSize.bottom = GetSystemMetrics(SM_CYVIRTUALSCREEN); // height of screen 1 (again i think)
+    desktopHandle = GetDesktopWindow();                       // gets the screen handle
+    desktop = GetDC(desktopHandle);                           // gets the device context aka the pixels of the screen
+    InvertRect(desktop, &screenSize);                         // inverts the screen
+    return ReleaseDC(desktopHandle, desktop);                 // returns 1 if releasing the dc worked
 }
 int main(int argc, char **argv)
 {
@@ -22,24 +22,24 @@ int main(int argc, char **argv)
     char key;
     if (argc > 1)
         key = *argv[1];
-    if (argc == 1) // command name
-        std::cout << "You have chosen no key for inverting the screen.\nIt will default to '" << defaultKey << "'.\nTo pass a different key, run this program in the command prompt\nand add a letter after it.\ne.g. 'InvertScreen.exe H' for the 'H' key to invert your screen." << std::flush;
+    if (argc == 1)                                                                                                                                                                                                                                                                                // command name
+        std::cout << "You have chosen no key for inverting the screen.\nIt will default to '" << defaultKey << "'.\nTo pass a different key, run this program in the command prompt\nand add a letter after it.\ne.g. 'InvertScreen.exe H' for the 'H' key to invert your screen." << std::flush; // std::flush so we can clear  the console later
     else if (argc > 1)
-        std::cout << "You have chosen '" << key << "' as your invert screen key.\nNow when you press '" << key << "', the screen will invert.\nIf it is already inverted, this will revert it back to the original colours.\nIf the key doesn't work, it may be an invalid key." << std::flush;
+        std::cout << "You have chosen '" << key << "' as your invert screen key.\nNow when you press '" << key << "', the screen will invert.\nIf it is already inverted, this will revert it back to the original colours.\nIf the key doesn't work, it may be an invalid key." << std::flush; // "invalid key" aka key that doesnt work with GetKeyState
     int keyDownY = 0;
     int keyEverPressed = 0;
     while (true)
     {
         if (key != 0)
         {
-            if ((GetKeyState(key) & 0x8000) && keyDownY != 1)
+            if ((GetKeyState(key) & 0x8000) && keyDownY != 1) // the actual key press part i found on stack overflow only part i yoinked
             {
                 keyDownY = 1;
                 invertScreen();
                 if (keyEverPressed == 0)
                 {
                     keyEverPressed = 1;
-                    system("CLS");
+                    system("CLS"); // clears the terminal
                 }
             }
             else if (!(GetKeyState(key) & 0x8000))
@@ -56,7 +56,7 @@ int main(int argc, char **argv)
                 if (keyEverPressed == 0)
                 {
                     keyEverPressed = 1;
-                    system("CLS");
+                    system("CLS"); // clears the terminal
                 }
             }
             else if (!(GetKeyState('Y') & 0x8000))
